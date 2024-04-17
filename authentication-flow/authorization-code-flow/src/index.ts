edc08e4253b45ff824222a14cafb1bacd4cc2163
moliveira <moliveira@pinterest.com>
2024-04-17 05:39:43 -0700

    feat: :sparkles: login using keycloak

--- authentication-flow/authorization-code-flow/src/index.ts
+++ authentication-flow/authorization-code-flow/src/index.ts
@@ -20,6 +20,33 @@ app.get('/login', (req, res) => {
 });
 
 
+app.get('/callback', async (req, res) => {
+    console.log(req.query);
+
+    const bodyParams = new URLSearchParams({
+        client_id: 'fullcycle-client',
+        grant_type: 'authorization_code',
+        code: req.query.code as string,
+        redirect_uri: 'http://localhost:3000/callback', 
+    });
+
+    const url = `http://host.docker.internal:8080/realms/fullcycle-realm/protocol/openid-connect/token`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: bodyParams.toString(),
+    });
+
+    const result = await response.json();
+
+    console.log(result);
+
+    res.json(result);
+});
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
