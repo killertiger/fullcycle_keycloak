de104dd4d69246338f74a3223460c6f81e4b2027
moliveira <moliveira@pinterest.com>
2024-04-18 04:20:53 -0700

    feat: :sparkles: Implementing nonce to avoid replay attack

--- authentication-flow/authorization-code-flow/src/index.ts
+++ authentication-flow/authorization-code-flow/src/index.ts
@@ -1,17 +1,39 @@
 import express from 'express';
+import session from 'express-session';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+
+declare module "express-session" {
+    interface SessionData {
+        nonce: string;
+    }
+  }
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
+const memoryStore = new session.MemoryStore();
+
+app.use(
+    session({
+        secret: 'my-secret',
+        resave: false,
+        saveUninitialized: false,
+        store: memoryStore,
+    })
+)
 
 app.get('/login', (req, res) => {
+    const nonce = crypto.randomBytes(16).toString('base64');
+
+    req.session.nonce = nonce;
+    req.session.save();
+
     const loginParams = new URLSearchParams({
         client_id: 'fullcycle-client',
         redirect_uri: 'http://localhost:3000/callback',
         response_type: 'code',
         scope: 'openid',
+        nonce,
     })
 
     const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginParams.toString()}`;
@@ -43,6 +65,18 @@ app.get('/callback', async (req, res) => {
     const result = await response.json();
 
     console.log(result);
+
+    const payloadAccessToken = jwt.decode(result.access_token) as any;
+    const payloadRefreshToken = jwt.decode(result.refresh_token) as any;
+    const payloadIdToken = jwt.decode(result.id_token) as any;
+
+    if(payloadAccessToken.nonce !== req.session.nonce
+        || payloadRefreshToken.nonce !== req.session.nonce
+        || payloadIdToken.nonce !== req.session.nonce
+    ) {
+        return res.status(401).send({message: 'Unauthenticated'});
+    }
+
 
     res.json(result);
 });
