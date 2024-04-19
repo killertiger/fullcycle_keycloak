11d5446b8cfcfd02a90fea5bd662e1a637e53919
moliveira <moliveira@pinterest.com>
2024-04-19 06:21:25 -0700

    feat: :sparkles: Improve callback implicit-flow

--- authentication-flow/implicit-flow/src/Callback.tsx
+++ authentication-flow/implicit-flow/src/Callback.tsx
@@ -1,7 +1,29 @@
+import { useLocation } from "react-router-dom"
+import { login } from "./utils";
+import { useEffect } from "react";
+
 export function Callback() {
+    const { hash } = useLocation();
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(hash.replace("#", ""));
+
+        const accessToken = searchParams.get("access_token") as string;
+        const idToken = searchParams.get("id_token") as string;
+        const state = searchParams.get("state") as string;
+
+        if (!accessToken || !idToken || !state) {
+
+        }
+
+        login(accessToken, idToken, state);
+
+        console.log("Logged in");
+    }, [hash]);
+
     return (
         <div>
-            <h1>Callback</h1>
+            Loading...
         </div>
     )
 }
--- authentication-flow/implicit-flow/src/utils.ts
+++ authentication-flow/implicit-flow/src/utils.ts
@@ -1,4 +1,5 @@
 import Cookies from "js-cookie";
+import { decodeJwt } from "jose";
 
 export function makeLoginUrl() {
     const nonce = Math.random().toString(36);
@@ -16,4 +17,34 @@ export function makeLoginUrl() {
     });
 
     return `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginUrlParams.toString()}`;
+}
+
+export function login(accessToken: string, idToken: string, state: string) {
+    const stateCookie = Cookies.get("state");
+    if (stateCookie !== state) {
+        throw new Error("Invalid state");
+    }
+
+    let decodedAccessToken = null;
+    let decodedIdToken = null;
+
+    try {
+        decodedAccessToken = decodeJwt(accessToken);
+        decodedIdToken = decodeJwt(idToken);
+    } catch (e) {
+        throw new Error("Invalid token");
+    }
+
+    if (decodedAccessToken.nonce !== Cookies.get("nonce")) {
+        throw new Error("Invalid nonce")
+    }
+
+    if(decodedIdToken.nonce !== Cookies.get("nonce")) {
+        throw new Error("Invalid nonce")
+    }
+
+    Cookies.set("access_token", accessToken);
+    Cookies.set("id_token", idToken);
+
+    return decodedAccessToken;
 }
